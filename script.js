7d547739047bf1d253d4ac2e2c550c4778d66b1c
plugwearstore <plugwearstore@gmail.com>
2026-05-14 17:56:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let cart = [];
+
 function addToCart(product){
-  alert(product + " added to cart");
+
+  cart.push(product);
+
+  document.getElementById("cart-count").innerText = cart.length;
+
+  let cartItems = document.getElementById("cart-items");
+
+  let item = document.createElement("p");
+
+  item.innerText = product;
+
+  cartItems.appendChild(item);
+}
+
+function openCart(){
+
+  document.getElementById("cart").classList.toggle("active");
+
 }
