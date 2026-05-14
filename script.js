3e277db08cf352cb8f13ed4788a74a38cc739f9a
plugwearstore <plugwearstore@gmail.com>
2026-05-14 19:16:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,29 @@
 const auth = firebase.auth();
-let cart = [];
 
-function addToCart(product){
+function registerUser(){
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
 
-  cart.push(product);
-
-  document.getElementById("cart-count").innerText = cart.length;
-
-  let cartItems = document.getElementById("cart-items");
-
-  let item = document.createElement("p");
-
-  item.innerText = product;
-
-  cartItems.appendChild(item);
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      alert("Account created 🔥");
+    })
+    .catch(err => alert(err.message));
 }
 
-function openCart(){
+function loginUser(){
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
 
-  document.getElementById("cart").classList.toggle("active");
-
-}
-function openLogin(){
-
-  document.getElementById("loginBox").classList.toggle("active");
-
-}
-
-function login(){
-document.querySelector(".checkout").onclick = function(){
-  window.location.href = "checkout.html";
-}
-  alert("Welcome to PLUGWEAR");
-  
-
-}
-function loadProducts(){
-
-  const container = document.getElementById("products");
-
-  db.collection("products").onSnapshot(snapshot => {
-
-    container.innerHTML = "";
-
-    snapshot.forEach(doc => {
-
-      let p = doc.data();
-
-      container.innerHTML += `
-        <div class="card">
-          <img src="${p.image}" />
-          <h2>${p.name}</h2>
-          <p>${p.price}€</p>
-          <button onclick="addToCart('${p.name}')">
-            Add To Cart
-          </button>
-        </div>
-      `;
-
-    });
-
-  });
-
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      alert("Logged in 🔥");
+      document.getElementById("loginBox").classList.remove("active");
+    })
+    .catch(err => alert(err.message));
 }
 
-loadProducts();
+function logout(){
+  auth.signOut();
+  alert("Logged out");
+}
